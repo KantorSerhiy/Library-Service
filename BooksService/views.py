59cf10225478dcce45d1fdad3116e7bbfed248c6
skantor <serhiykantor@gmail.com>
2023-02-26 21:15:29 +0200

    add option Only admin users can create/update/delete books

--- BooksService/views.py
+++ BooksService/views.py
@@ -3,8 +3,11 @@ from rest_framework import viewsets
 
 from BooksService.models import Book
 from BooksService.serializers import BookSerializer
+from BooksService.permissions import IsAdminUserOrReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
